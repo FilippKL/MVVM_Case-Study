c8bc44f33a8f7d2a915b2c969cd1673f92928efc
“Filipp2911” <krupnov_f@auca.kg>
2021-07-06 17:31:26 +0600

    Initial Commit

---
+++ MVVM_Case-Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Case-Study
+//
+//  Created by Filipp Krupnov on 6/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
