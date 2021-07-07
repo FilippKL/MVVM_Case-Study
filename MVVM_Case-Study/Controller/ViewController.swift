1257b3d0a10e041444f10e540386aff57f74aa7a
“Filipp2911” <krupnov_f@auca.kg>
2021-07-07 16:42:28 +0600

    extension

--- MVVM_Case-Study/Controller/ViewController.swift
+++ MVVM_Case-Study/Controller/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
     
     var words = [String]()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         words = [""]
@@ -29,42 +28,3 @@ class ViewController: UIViewController {
         }
     }
 }
-extension ViewController : UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return words.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "wordCell", for: indexPath)
-        cell.textLabel?.text = words[indexPath.row]
-//        print("\(userName) index")
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.allowsSelection = true
-        let cell = tableView.dequeueReusableCell(withIdentifier: "wordCell", for: indexPath)
-        cell.textLabel?.text = words[indexPath.row]
-        userN = words[indexPath.row]
-        print("\(userN) user1")
-        
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            words.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-    
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? detailedViewController {
-            destination.words1 = words[(wordsTableView.indexPathForSelectedRow?.row)!]
-            wordsTableView.deselectRow(at: wordsTableView.indexPathForSelectedRow!, animated: true)
-            
-        }
-  }
-    
-}
-
--- MVVM_Case-Study/ViewModel/Parser.swift
+++ MVVM_Case-Study/ViewModel/Parser.swift
@@ -9,15 +9,13 @@ import Foundation
 
 var sumStar : Int = 0
 var sumIssues: Int = 0
-//var words1 : String = ""
 
 
 struct Parser {
     
     func requestData() {
-//        print("\(words1) gjdnc")
         
-        let urlComponents = URLComponents(string: "https://api.github.com/users/\(userN)/repos") //\(userNameVar.newUserName)/repos")
+        let urlComponents = URLComponents(string: "https://api.github.com/users/\(userN)/repos")
         let wordsRequest = URLRequest(url: urlComponents!.url!)
     
         let sharedSession = URLSession.shared
@@ -28,13 +26,12 @@ struct Parser {
                (200..<300) ~= response.statusCode,
                error == nil,
                let requestedWord = try? JSONDecoder().decode([Request].self, from: data){
-                print(requestedWord)
+//                print(requestedWord)
                 
                 sumStar = requestedWord.map({ $0.stargazers_count }).reduce(0, +)
                 sumIssues = requestedWord.map({ $0.open_issues_count }).reduce(0, +)
                 print(sumStar, sumIssues)
                 DispatchQueue.main.async {
-//                    updateLbl()
                         }
                     }
         }
